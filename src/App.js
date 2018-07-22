c491a82592f02d7a8937fa769d7c955f135aadd2
tomato <important1105@outlook.jp>
2018-07-22 20:14:43 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  render() {
-    const style = {
-      color: "red"
-    }
-    return (
-      <React.Fragment>
-        <div style={style}>
-          Hello, World
-        </div>
-       <div style={style}>
-         Hello, World
-       </div>
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     const style = {
+//       color: "red"
+//     }
+//     return (
+//       <React.Fragment>
+//         <div style={style}>
+//           Hello, World
+//         </div>
+//        <div style={style}>
+//          Hello, World
+//        </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Tom", age: 20 },
+    { name: "HogeHoge"}
+  ]
+  return(
+    <div>
+      {
+        profiles.map((p, i) => {
+          return <User name={p.name} age={p.age} key={i}></User>
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Name: {props.name}, Age: {props.age}</div>
+}
+
+User.propTypes = {//型チェックできる
+  name: PropTypes.string,
+  // age: PropTypes.number//ageを記述してないことに対するエラーは表示されない
+  age: PropTypes.number.isRequired//ageは必須になる
 }
 
 export default App;
